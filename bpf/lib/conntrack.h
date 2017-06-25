craigbox/cilium

commit e9f256ad8ad4218ccf141feb544c05e930197c11

Initial conntracking implementation
Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/lib/conntrack.h
@@ -1,0 +1,117 @@
+#ifndef __LIB_CONNTRACK_H_
+#define __LIB_CONNTRACK_H_
+
+#include "common.h"
+#include "ipv6.h"
+#include "dbg.h"
+
+#define CT_INVALID	0
+#define CT_NEW		1
+#define CT_REPLY	2
+
+static inline int __inline__ ct_lookup6(struct __sk_buff *skb, int off,
+				        struct ipv6_ct_tuple *tuple)
+{
+	struct ipv6_ct_entry *entry;
+	__u8 nexthdr = tuple->nexthdr;
+	__u8 state = CT_NEW;
+
+	switch (tuple->nexthdr) {
+	case IPPROTO_ICMPV6:
+		/* ICMPv6 will trigger a lookup with ports and nexthdr set to 0. The
+		 * conntrack table will contain both an entry with and wihout L4 bits
+		 * to allow matching related ICMP.
+		 */
+		tuple->nexthdr = 0;
+		break;
+
+	case IPPROTO_TCP:
+		if (tcp_load_sport(skb, off, &tuple->sport) < 0 ||
+		    tcp_load_dport(skb, off, &tuple->dport) < 0)
+			return CT_INVALID;
+		break;
+
+	case IPPROTO_UDP:
+		if (udp_load_sport(skb, off, &tuple->sport) < 0 ||
+		    udp_load_dport(skb, off, &tuple->dport) < 0)
+			return CT_INVALID;
+		break;
+	}
+
+	printk("CT lookup for ctx=%d sport=%d dport=%d\n",
+		tuple->secctx, tuple->sport, tuple->dport);
+
+	entry = map_lookup_elem(&cilium_ct, tuple);
+	if (entry) {
+		printk("CT entry found\n");
+		entry->last_activity = ktime_get_ns();
+		state = CT_REPLY;
+
+		if (nexthdr == IPPROTO_ICMPV6)
+			tuple->nexthdr = nexthdr;
+	}
+
+	return state;
+}
+
+/* Offset must point to IPv6 */
+static inline int __inline__ ct_create6(struct __sk_buff *skb, int off, __u32 secctx)
+{
+	struct ipv6_ct_tuple reversed = {
+		.secctx = secctx,
+	};
+
+	if (ipv6_load_nexthdr(skb, off, &reversed.nexthdr) < 0 ||
+	    ipv6_load_saddr(skb, off, &reversed.dst) < 0 ||
+	    ipv6_load_daddr(skb, off, &reversed.src) < 0)
+		return TC_ACT_SHOT;
+
+	off += sizeof(struct ipv6hdr);
+
+	switch (reversed.nexthdr) {
+	case IPPROTO_ICMPV6:
+		break;
+
+	case IPPROTO_TCP:
+		if (tcp_load_sport(skb, off, &reversed.dport) < 0 ||
+		    tcp_load_dport(skb, off, &reversed.sport) < 0)
+			return TC_ACT_SHOT;
+		break;
+
+	case IPPROTO_UDP:
+		if (udp_load_sport(skb, off, &reversed.dport) < 0 ||
+		    udp_load_dport(skb, off, &reversed.sport) < 0)
+			return TC_ACT_SHOT;
+		break;
+
+	default:
+		/* Can't handle extension headers yet */
+		return TC_ACT_SHOT;
+	}
+
+	if (1) {
+		struct ipv6_ct_entry entry = {};
+		entry.last_activity = ktime_get_ns();
+
+		printk("CT entry updated for secctx=%d sport=%d dport=%d\n",
+			secctx, reversed.sport, reversed.dport);
+
+		if (reversed.nexthdr == IPPROTO_ICMPV6)
+			reversed.nexthdr = 0;
+
+		map_update_elem(&cilium_ct, &reversed, &entry, 0);
+
+		if (reversed.nexthdr != IPPROTO_ICMPV6) {
+			reversed.nexthdr = 0;
+			reversed.sport = 0;
+			reversed.dport = 0;
+
+			map_update_elem(&cilium_ct, &reversed, &entry, 0);
+		}
+
+	}
+
+	return 0;
+}
+
+#endif

bpf/bpf_lxc.c
@@ -17,6 +17,7 @@
 #include "lib/l3.h"
 #include "lib/lxc.h"
 #include "lib/nat46.h"
+#include "lib/conntrack.h"
 
 #ifdef DROP_NOTIFY
 #include "lib/drop.h"
@@ -46,20 +47,22 @@ static inline void map_lxc_out(struct __sk_buff *skb, int off)
 }
 #endif /* DISABLE_PORT_MAP */
 
-static inline int __inline__ do_l3_from_lxc(struct __sk_buff *skb, int nh_off)
+static inline int __inline__ do_l3_from_lxc(struct __sk_buff *skb, int nh_off,
+					    struct ipv6_ct_tuple *tuple)
 {
-	union v6addr dst = {};
 	__u32 node_id;
 	int to_host = 0, do_nat46 = 0;
 
-	printk("L3 from lxc: skb %p len %d\n", skb, skb->len);
+	printk("L3 from lxc len=%d\n", skb->len);
 
-	if (verify_src_mac(skb) || verify_src_ip(skb, nh_off) ||
-	    verify_dst_mac(skb))
+	if (ipv6_load_saddr(skb, nh_off, &tuple->src) < 0 ||
+	    ipv6_load_daddr(skb, nh_off, &tuple->dst) < 0)
 		return TC_ACT_SHOT;
 
-	ipv6_load_daddr(skb, nh_off, &dst);
-	node_id = ipv6_derive_node_id(&dst);
+	if (verify_src_mac(skb) || verify_src_ip(tuple) || verify_dst_mac(skb))
+		return TC_ACT_SHOT;
+
+	node_id = ipv6_derive_node_id(&tuple->dst);
 
 #ifndef DISABLE_PORT_MAP
 	map_lxc_out(skb, nh_off);
@@ -72,7 +75,7 @@ static inline int __inline__ do_l3_from_lxc(struct __sk_buff *skb, int nh_off)
 		union v6addr host_ip = HOST_IP;
 
 		/* Packets to the host are punted to a dummy device */
-		if (ipv6_addrcmp(&dst, &host_ip) == 0)
+		if (ipv6_addrcmp(&tuple->dst, &host_ip) == 0)
 			to_host = 1;
 	}
 #endif
@@ -81,7 +84,7 @@ static inline int __inline__ do_l3_from_lxc(struct __sk_buff *skb, int nh_off)
 	if (1) {
 		/* FIXME: Derive from prefix constant */
 		__u32 p = 0;
-		p = dst.p1 & 0xffff;
+		p = tuple->dst.p1 & 0xffff;
 		if (p == 0xadde) {
 			to_host = 1;
 			do_nat46 = 1;
@@ -89,9 +92,23 @@ static inline int __inline__ do_l3_from_lxc(struct __sk_buff *skb, int nh_off)
 	}
 #endif
 
+#ifndef DISABLE_CONNTRACK
+	/* Create connection tracking entry for all outgoing packets so replies
+	 * can be related. */
+	ct_create6(skb, nh_off, SECLABEL);
+#endif
+
 	if (unlikely(to_host)) {
 		union macaddr router_mac = NODE_MAC, host_mac = HOST_IFINDEX_MAC;
 		int ret;
+
+		tuple->secctx = HOST_ID;
+
+#ifndef DISABLE_CONNTRACK
+		skb->cb[CB_STATE] = ct_lookup6(skb, nh_off, tuple);
+		if (skb->cb[CB_STATE] == CT_INVALID)
+			return TC_ACT_SHOT;
+#endif /* DISABLE_CONNTRACK */
 
 		ret = __do_l3(skb, nh_off, (__u8 *) &router_mac.addr, (__u8 *) &host_mac.addr);
 		if (ret != TC_ACT_OK)
@@ -139,15 +156,17 @@ static inline int __inline__ do_l3_from_lxc(struct __sk_buff *skb, int nh_off)
 		return TC_ACT_OK;
 #endif
 	} else {
-		return do_l3(skb, nh_off, &dst, SECLABEL);
+		return do_l3_ct(skb, nh_off, tuple);
 	}
 }
 
 __section("from-container")
 int handle_ingress(struct __sk_buff *skb)
 {
+	struct ipv6_ct_tuple tuple = {
+		.secctx = SECLABEL,
+	};
 	int ret, nh_off = ETH_HLEN;
-	__u8 nexthdr;
 
 	/* Drop all non IPv6 traffic */
 	if (unlikely(skb->protocol != __constant_htons(ETH_P_IPV6)))
@@ -156,15 +175,15 @@ int handle_ingress(struct __sk_buff *skb)
 	/* Handle ICMPv6 messages to the logical router, all other ICMPv6
 	 * messages are passed on to the container (REDIRECT_TO_LXC)
 	 */
-	nexthdr = load_byte(skb, nh_off + offsetof(struct ipv6hdr, nexthdr));
-	if (unlikely(nexthdr == IPPROTO_ICMPV6)) {
+	tuple.nexthdr = load_byte(skb, nh_off + offsetof(struct ipv6hdr, nexthdr));
+	if (unlikely(tuple.nexthdr == IPPROTO_ICMPV6)) {
 		ret = icmp6_handle(skb, nh_off);
 		if (ret != REDIRECT_TO_LXC)
 			return ret;
 	}
 
 	/* Perform L3 action on the frame */
-	return do_l3_from_lxc(skb, nh_off);
+	return do_l3_from_lxc(skb, nh_off, &tuple);
 }
 
 __BPF_MAP(POLICY_MAP, BPF_MAP_TYPE_HASH, 0, sizeof(__u32),
@@ -177,6 +196,11 @@ __section_tail(CILIUM_MAP_JMP, SECLABEL) int handle_policy(struct __sk_buff *skb
 #ifndef DISABLE_POLICY_ENFORCEMENT
 	struct policy_entry *policy;
 	__u32 src_label = skb->cb[0];
+
+#ifndef DISABLE_CONNTRACK
+	if (skb->cb[CB_STATE] == CT_REPLY)
+		goto allow;
+#endif /* DISABLE_CONNTRACK */
 
 	policy = map_lookup_elem(&POLICY_MAP, &src_label);
 	if (unlikely(!policy)) {
@@ -192,6 +216,13 @@ __section_tail(CILIUM_MAP_JMP, SECLABEL) int handle_policy(struct __sk_buff *skb
 	}
 	__sync_fetch_and_add(&policy->packets, 1);
 	__sync_fetch_and_add(&policy->bytes, skb->len);
+
+#ifndef DISABLE_CONNTRACK
+	/* Create connection tracking entry in reverse direction */
+	ct_create6(skb, ETH_HLEN, SECLABEL);
+allow:
+#endif /* DISABLE_CONNTRACK */
+
 #endif
 
 	return redirect(ifindex, 0);

bpf/bpf_netdev.c
@@ -18,6 +18,11 @@
 #include "lib/l3.h"
 #include "lib/nat46.h"
 #include "lib/arp.h"
+#include "lib/conntrack.h"
+
+#ifndef SECLABEL
+#define SECLABEL WORLD_ID
+#endif
 
 #ifdef DROP_NOTIFY
 #include "lib/drop.h"
@@ -76,20 +81,17 @@ __section_tail(CILIUM_MAP_PROTO, CILIUM_MAP_PROTO_ARP) int arp_respond(struct __
 	return redirect(skb->ifindex, 0);
 }
 
-static inline __u32 derive_sec_ctx(struct __sk_buff *skb, const union v6addr *node_ip)
+static inline __u32 derive_sec_ctx(struct __sk_buff *skb, struct ipv6_ct_tuple *tuple,
+				   const union v6addr *node_ip)
 {
 #ifdef FIXED_SRC_SECCTX
 	__u32 flowlabel = FIXED_SRC_SECCTX;
 #else
-	__u32 flowlabel = 0;
-	union v6addr src = {};
+	__u32 flowlabel = WORLD_ID;
 
-	ipv6_load_saddr(skb, ETH_HLEN, &src);
-	if (matches_cluster_prefix(&src, node_ip)) {
+	if (matches_cluster_prefix(&tuple->src, node_ip)) {
 		ipv6_load_flowlabel(skb, ETH_HLEN, &flowlabel);
 		flowlabel = ntohl(flowlabel);
-	} else {
-		flowlabel = WORLD_ID;
 	}
 #endif
 
@@ -124,7 +126,7 @@ int from_netdev(struct __sk_buff *skb)
 			return TC_ACT_OK;
 
 		if (ipv4_to_ipv6(skb, 14, &sp, &dp) < 0) {
-			printk("ipv4_to_ipv6 failed\n");
+			//printk("ipv4_to_ipv6 failed\n");
 			return TC_ACT_SHOT;
 		}
 		skb->tc_index = 1;
@@ -132,28 +134,25 @@ int from_netdev(struct __sk_buff *skb)
 #endif
 
 	if (likely(skb->protocol == __constant_htons(ETH_P_IPV6))) {
-		union v6addr dst = {};
-		__u32 flowlabel;
+		struct ipv6_ct_tuple tuple = {};
 
+		tuple.nexthdr = load_byte(skb, ETH_HLEN + offsetof(struct ipv6hdr, nexthdr));
 #ifdef HANDLE_NS
-		__u8 nexthdr;
-
-		nexthdr = load_byte(skb, ETH_HLEN + offsetof(struct ipv6hdr, nexthdr));
-		if (unlikely(nexthdr == IPPROTO_ICMPV6)) {
+		if (unlikely(tuple.nexthdr == IPPROTO_ICMPV6)) {
 			int ret = icmp6_handle(skb, ETH_HLEN);
 			if (ret != REDIRECT_TO_LXC)
 				return ret;
 		}
 #endif
-		printk("IPv6 packet from netdev skb %p len %d\n", skb, skb->len);
+		printk("IPv6 from netdev len=%d\n", skb->len);
 
-		ipv6_load_daddr(skb, ETH_HLEN, &dst);
-		flowlabel = derive_sec_ctx(skb, &node_ip);
+		ipv6_load_saddr(skb, ETH_HLEN, &tuple.src);
+		ipv6_load_daddr(skb, ETH_HLEN, &tuple.dst);
+		tuple.secctx = derive_sec_ctx(skb, &tuple, &node_ip);
 
-		if (likely(is_node_subnet(&dst, &node_ip))) {
-			printk("Targeted for a local container, src label: %d\n", flowlabel);
-
-			return do_l3(skb, ETH_HLEN, &dst, flowlabel);
+		if (is_node_subnet(&tuple.dst, &node_ip)) {
+			printk("Local container target, srcctx=%d\n", tuple.secctx);
+			return do_l3_ct(skb, ETH_HLEN, &tuple);
 		}
 	}
 
@@ -173,6 +172,11 @@ __section_tail(CILIUM_MAP_JMP, SECLABEL) int handle_policy(struct __sk_buff *skb
 
 	printk("Handle for host %d %d\n", src_label, ifindex);
 
+#ifndef DISABLE_CONNTRACK
+	if (skb->cb[CB_STATE] == CT_REPLY)
+		goto allow;
+#endif /* DISABLE_CONNTRACK */
+
 	policy = map_lookup_elem(&POLICY_MAP, &src_label);
 	if (!policy) {
 #ifdef DROP_NOTIFY
@@ -187,6 +191,13 @@ __section_tail(CILIUM_MAP_JMP, SECLABEL) int handle_policy(struct __sk_buff *skb
 	}
 	__sync_fetch_and_add(&policy->packets, 1);
 	__sync_fetch_and_add(&policy->bytes, skb->len);
+
+#ifndef DISABLE_CONNTRACK
+	/* Create connection tracking entry in reverse direction */
+	ct_create6(skb, ETH_HLEN, SECLABEL);
+allow:
+#endif /* DISABLE_CONNTRACK */
+
 #endif
 	return redirect(ifindex, 0);
 }

bpf/lib/common.h
@@ -71,9 +71,26 @@ struct drop_notify {
 	char		data[DROP_SAMPLE_LEN];
 };
 
+struct ipv6_ct_tuple {
+	union v6addr src;
+	union v6addr dst;
+	__u32 secctx;
+	__u16 sport;
+	__u16 dport;
+	__u8 nexthdr;
+};
+
+struct ipv6_ct_entry {
+	__u64	last_activity;
+};
+
 #ifndef BPF_F_PSEUDO_HDR
 # define BPF_F_PSEUDO_HDR                (1ULL << 4)
 #endif
+
+enum {
+	CB_STATE = 2,
+};
 
 /* Cilium error codes, must NOT overlap with TC return codes */
 #define REDIRECT_TO_LXC		-257

bpf/lib/l3.h
@@ -9,6 +9,7 @@
 #include "lxc_map.h"
 #include "icmp6.h"
 #include "geneve.h"
+#include "conntrack.h"
 
 /* validating options on tx is optional */
 #define VALIDATE_GENEVE_TX
@@ -94,7 +95,7 @@ static inline int __inline__ do_l3(struct __sk_buff *skb, int nh_off,
 	__u16 lxc_id = derive_lxc_id(dst);
 	int ret = 0;
 
-	printk("Local L3 - lxc-id: %x\n", lxc_id);
+	printk("L3 to lxc-id: %x\n", lxc_id);
 
 	dst_lxc = map_lookup_elem(&cilium_lxc, &lxc_id);
 	if (dst_lxc) {
@@ -110,13 +111,63 @@ static inline int __inline__ do_l3(struct __sk_buff *skb, int nh_off,
 			map_lxc_in(skb, nh_off, dst_lxc);
 #endif /* DISABLE_PORT_MAP */
 
-		printk("L3 to ifindex %u ID: %d\n",
+		printk("Target ifindex %u dstctx=%d\n",
 			dst_lxc->ifindex, ntohl(dst_lxc->sec_label));
 
 		skb->cb[0] = seclabel;
 		skb->cb[1] = dst_lxc->ifindex;
 
 		tail_call(skb, &cilium_jmp, ntohl(dst_lxc->sec_label));
+		printk("No policy program found, dropping\n");
+		return TC_ACT_SHOT;
+	} else {
+		printk("No match\n");
+	}
+
+	return TC_ACT_UNSPEC;
+}
+
+static inline int __inline__ do_l3_ct(struct __sk_buff *skb, int nh_off,
+				      struct ipv6_ct_tuple *tuple)
+{
+	struct lxc_info *dst_lxc;
+	__u16 lxc_id = derive_lxc_id(&tuple->dst);
+	int ret = 0;
+
+	printk("Local L3 - dst id: %x\n", lxc_id);
+
+	dst_lxc = map_lookup_elem(&cilium_lxc, &lxc_id);
+	if (dst_lxc) {
+		mac_t lxc_mac = dst_lxc->mac;
+		mac_t router_mac = dst_lxc->node_mac;
+		__u32 secctx = ntohl(dst_lxc->sec_label);
+
+		/* FIXME: clean this up */
+		skb->cb[0] = tuple->secctx;
+		tuple->secctx = secctx;
+
+#ifndef DISABLE_CONNTRACK
+		skb->cb[CB_STATE] = ct_lookup6(skb, nh_off, tuple);
+		if (skb->cb[CB_STATE] == CT_INVALID)
+			return TC_ACT_SHOT;
+#endif /* !DISABLE_CONNTRACK */
+
+		ret = __do_l3(skb, nh_off, (__u8 *) &router_mac, (__u8 *) &lxc_mac);
+		if (ret != TC_ACT_OK)
+			return ret;
+
+#ifndef DISABLE_PORT_MAP
+		if (dst_lxc->portmap[0].to)
+			map_lxc_in(skb, nh_off, dst_lxc);
+#else
+		//printk("Port mapping disabled, skipping.\n");
+#endif /* DISABLE_PORT_MAP */
+
+		printk("L3 to ifindex %u ID: %d\n", dst_lxc->ifindex, secctx);
+
+		skb->cb[1] = dst_lxc->ifindex;
+
+		tail_call(skb, &cilium_jmp, secctx);
 		printk("No policy program found, dropping\n");
 		return TC_ACT_SHOT;
 	} else {

bpf/lib/lxc.h
@@ -27,20 +27,19 @@ static inline int verify_src_mac(struct __sk_buff *skb)
 #endif
 
 #ifndef DISABLE_SIP_VERIFICATION
-static inline int verify_src_ip(struct __sk_buff *skb, int off)
+static inline int verify_src_ip(struct ipv6_ct_tuple *tuple)
 {
-	union v6addr src = {}, valid = LXC_IP;
+	union v6addr valid = LXC_IP;
 	int ret;
 
-	ipv6_load_saddr(skb, off, &src);
-	ret = ipv6_addrcmp(&src, &valid);
+	ret = ipv6_addrcmp(&tuple->src, &valid);
 	if (unlikely(ret))
 		printk("Invalid source IP address, dropping...\n");
 
 	return ret;
 }
 #else
-static inline int verify_src_ip(struct __sk_buff *skb, int off)
+static inline int verify_src_ip(struct ipv6_ct_tuple *tuple)
 {
 	return 0;
 }

bpf/lib/lxc_map.h
@@ -6,8 +6,13 @@
 #define CILIUM_MAP_LXC   0
 #define CILIUM_MAP_JMP   1
 #define CILIUM_MAP_PROTO 2
+#define CILIUM_MAP_CT    3
 
 __BPF_MAP(cilium_lxc, BPF_MAP_TYPE_HASH, CILIUM_MAP_LXC, sizeof(__u16), sizeof(struct lxc_info), PIN_GLOBAL_NS, 1024);
 BPF_PROG_ARRAY(cilium_jmp, CILIUM_MAP_JMP, PIN_GLOBAL_NS, 1024);
 BPF_PROG_ARRAY(cilium_proto, CILIUM_MAP_PROTO, PIN_GLOBAL_NS, 8);
+
+__BPF_MAP(cilium_ct, BPF_MAP_TYPE_HASH, CILIUM_MAP_CT, sizeof(struct ipv6_ct_tuple),
+	  sizeof(struct ipv6_ct_entry), PIN_GLOBAL_NS, 1024);
+
 #endif

bpf/lib/nat46.h
@@ -9,7 +9,7 @@
 #include "eth.h"
 #include "dbg.h"
 
-#define ENABLE_NAT46
+// #define ENABLE_NAT46
 
 static inline int get_csum_offset(__u8 protocol)
 {

commit 97f2a20441c02b4c3a41b95cf1567e89617af78c

bpf: Only create conntrack entries for TCP SYN packets
Fixes: e9f256ad8ad4218 ("Initial conntracking implementation")

Signed-off-by: Thomas Graf <thomas@cilium.io>

bpf/lib/conntrack.h
@@ -231,7 +231,11 @@ static inline int __inline__ ct_lookup6(void *map, struct ipv6_ct_tuple *tuple,
 				/* FIXME: Drop packets here with missing ACK flag? */
 			}
 		}
-		/* fall through */
+
+		/* load sport + dport into tuple */
+		if (skb_load_bytes(skb, l4_off, &tuple->dport, 4) < 0)
+			return DROP_CT_INVALID_HDR;
+		break;
 
 	case IPPROTO_UDP:
 		/* load sport + dport into tuple */
@@ -383,7 +387,11 @@ static inline int __inline__ ct_lookup4(void *map, struct ipv4_ct_tuple *tuple,
 				/* FIXME: Drop packets here with missing ACK flag? */
 			}
 		}
-		/* fall through */
+
+		/* load sport + dport into tuple */
+		if (skb_load_bytes(skb, off, &tuple->dport, 4) < 0)
+			return DROP_CT_INVALID_HDR;
+		break;
 
 	case IPPROTO_UDP:
 		/* load sport + dport into tuple */

daemon/Makefile
@@ -8,7 +8,7 @@ GO_BINDATA := go-bindata -prefix ../ -mode 0640 -modtime 1450269211 \
 	-ignore Makefile -ignore bpf_features.h -ignore lxc_config.h \
 	-ignore netdev_config.h -ignore node_config.h -ignore '.+\.o$$' \
 	-ignore '.+\.orig$$' -ignore '.+~$$' -ignore '\.DS_Store'
-GO_BINDATA_SHA1SUM=2f41034e602acf3a29ec557d981c39a6a1f44fce
+GO_BINDATA_SHA1SUM=41a8ce3a9fd5e0784f4879aca9e825da444c034a
 # We keep track of the go version used to update bindata in case of failures.
 # Having the version will likely be useful in the cases where someone is using
 # a older or newer version than the CI.